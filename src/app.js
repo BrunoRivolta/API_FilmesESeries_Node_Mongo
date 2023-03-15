4ce033a5fda38a9d173052127397787fefb24994
Bruno Rivolta <brrivolta@gmail.com>
2023-03-15 06:52:16 -0300

    Feito primeiro PUT em filmes

--- src/app.js
+++ src/app.js
@@ -5,8 +5,14 @@ const app = express()
 app.use(express.json())
 
 const filmes = [
-    {filme: 'a novissa rebelde'},
-    {filme: 'e o vento levou'}
+    {
+        id: 1,
+        filme: 'a novissa rebelde'
+    },
+    {
+        id: 2,
+        filme: 'e o vento levou'
+    }
 ]
 
 app.get('/', (req, res) => {
@@ -20,6 +26,13 @@ app.get('/filmes', (req, res) => {
 app.post('/filmes', (req, res) => {
     filmes.push(req.body)
     res.status(201).send('Livro cadastrado com sucesso')
+})
+
+app.put('/filmes/:id', async (req, res) => {
+    const id = req.params.id
+    const index = await filmes.findIndex(item => item.id == id)
+    filmes[index].filme = req.body.filme
+    res.status(200).json(filmes[index])
 })
 
 
