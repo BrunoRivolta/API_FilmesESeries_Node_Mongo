1a00322cbb07a0a6af13357d62a369cf3ae1ec86
Bruno Rivolta <brrivolta@gmail.com>
2023-03-15 06:28:14 -0300

    Feito primeiro POST em filmes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express'
 
 const app = express()
 
+app.use(express.json())
+
 const filmes = [
     {filme: 'a novissa rebelde'},
     {filme: 'e o vento levou'}
@@ -14,5 +16,11 @@ app.get('/', (req, res) => {
 app.get('/filmes', (req, res) => {
     res.status(200).json(filmes)
 })
+
+app.post('/filmes', (req, res) => {
+    filmes.push(req.body)
+    res.status(201).send('Livro cadastrado com sucesso')
+})
+
 
 export default app
