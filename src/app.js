dabaf849f3443f15f2cb8dcf79cf511e81de8376
Bruno Rivolta <brrivolta@gmail.com>
2023-03-15 07:00:28 -0300

    Feito primeiro DELETE em filmes

--- src/app.js
+++ src/app.js
@@ -23,16 +23,36 @@ app.get('/filmes', (req, res) => {
     res.status(200).json(filmes)
 })
 
+app.get('/filmes/:id', async (req, res) => {
+    const id = req.params.id
+    const index = await filmes.findIndex(item => item.id == id)
+    res.status(200).json(filmes[index])
+})
+
 app.post('/filmes', (req, res) => {
     filmes.push(req.body)
     res.status(201).send('Livro cadastrado com sucesso')
 })
 
 app.put('/filmes/:id', async (req, res) => {
-    const id = req.params.id
+    const {id} = req.params
     const index = await filmes.findIndex(item => item.id == id)
     filmes[index].filme = req.body.filme
     res.status(200).json(filmes[index])
+})
+
+app.put('/filmes/:id', async (req, res) => {
+    const {id} = req.params
+    const index = await filmes.findIndex(item => item.id == id)
+    filmes[index].filme = req.body.filme
+    res.status(200).json(filmes[index])
+})
+
+app.delete('/filmes/:id', async (req, res) => {
+    const {id} = req.params
+    const index = await filmes.findIndex(item => item.id == id)
+    filmes.splice(index, 1)
+    res.status(200).send(`Filmes ${id} Apagado com sucesso`)
 })
 
 
