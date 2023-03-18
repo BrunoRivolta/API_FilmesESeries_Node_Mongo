d3b8a0227754d34fcb1fa1d58c5cb0023b30ffe1
BrunoRivolta <brrivolta@gmail.com>
2023-03-18 15:13:10 -0300

    Terminado com sucesso os  testes na rotas de canais

--- src/Controllers/channelsController.js
+++ src/Controllers/channelsController.js
@@ -23,6 +23,40 @@ class ChannelsController {
         }
     }
 
+    static addChannel = async (req, res) => {
+        try {
+            const newChannel = new channels(req.body)
+            newChannel.save()
+            res.status(201).json({message: "Canal cadastrado com sucesso"})
+        } catch (error) {
+            console.log(error)
+            res.status(500).send('Erro no servidor!')
+        }
+    }
+
+    static updateChannel = async (req, res) => {
+        try {
+            const id = req.params.id
+            const update = req.body
+            const updateChannel = await channels.findOneAndUpdate({_id: id}, update)
+            res.status(200).json({message: "Canal atualizado com sucesso"}) 
+        } catch (error) {
+            console.log(error)
+            res.status(500).send('Erro no servidor!')
+        }
+    }
+
+    static deleteChannel = async (req, res) => {
+        try {
+            const id = req.params.id
+            const deleteChannel = await channels.findOneAndDelete({_id: id})
+            res.status(200).json({message: `Canal id: ${id}. Apagado com sucesso`})
+        } catch (error) {
+            console.log(error)
+            res.status(500).send('Erro no servidor!')
+        }
+    }
+
 }
 
 export default ChannelsController
--- src/routes/channelsRoutes.js
+++ src/routes/channelsRoutes.js
@@ -6,6 +6,10 @@ const router = express.Router()
 router
     .get('/canais', ChannelsController.listChannels)
     .get('/canais/:id', ChannelsController.listChannelById)
+    .post('/canais', ChannelsController.addChannel)
+    .put('/canais/:id', ChannelsController.updateChannel)
+    .delete('/canais/:id',ChannelsController.deleteChannel)
 
 export default router
+
 
